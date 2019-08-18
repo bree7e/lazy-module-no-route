bdcee3b8a30d440910effe18016ea94a9c0aabc5
Alexandr Vetrov <vetrov@icc.ru>
2019-08-19 00:03:47 +0800

    feat: generate components

---
+++ src/app/lazy/lazy.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { EntryComponent } from './entry/entry.component';
+
+@NgModule({
+  declarations: [OneComponent, TwoComponent],
+  entryComponents: [EntryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LazyModule { }
